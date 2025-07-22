ac8a1a47521937d3db94cbbc0b9d86fd5dcd5878
Yash kokate <yashkokate315@gmail.com>
2025-07-22 20:48:24 +0530

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -16,6 +16,13 @@ export const experiences: Experience[] = [
     period: "Aug 2023 - Dec 2023",
     description: "Spearheaded secondary research and delivered accurate, high-quality reports within deadlines. Analyzed companies, markets, and sectors including healthcare, consumer goods, financial services, and fintech. Prepared detailed company profiles with financial analysis, data visualization using Python and Matplotlib, SWOT, and strategy evaluations. Utilized databases such as D&B and Lexis Nexis for qualitative and quantitative research. Gained experience in interpreting data and presenting insights tailored to client requirements.",
     side: "right"
+  },
+    {
+    title: "Founder",
+    company: "Independent Traders Academy",
+    period: "Jan 2020 - Apr 2023",
+    description: "I founded and grew an educational academy to empower over 500 students, using hands-on workshops and live trading events to break down complex financial concepts into practical, actionable knowledge. I designed and led interactive programs that brought real-world scenarios into the classroom, cultivating essential skills for success. By developing original content and dynamic presentations, I engaged audiences from diverse backgrounds, fostering a vibrant community of learners. I also established and managed partnerships with both academic and informal groups, coordinating a variety of events—from peer-led discussions to strategy competitions—that encouraged collaboration, mentorship, and shared growth. My journey reflects a passion for education outreach, event management, curriculum development, and community building, underpinned by strong skills in leadership, communication, and content creation.",
+    side: "right"
   }
 ];
 
